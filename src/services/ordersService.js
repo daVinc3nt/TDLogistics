1713d964fa98c0c0c8d0c017031a1c5f3f47ffae
nhan6104 <139254987+nhan6104@users.noreply.github.com>
2024-01-02 16:53:07 +0700

    Update ordersService.js
    cancelOrder,
        getTime,
        checkExistOrder,

--- src/services/ordersService.js
+++ src/services/ordersService.js
@@ -11,9 +11,25 @@ const getAllOrders = async () => {
 const getOrder = async (fields, values) => {
     return await Orders.getOrder(fields, values);
 };
+const cancelOrder=async (phoneNumber)=>
+{
+    await Users.cancelOrder(phoneNumber);
+};
 
+const getTime = async (phoneNumber)=>
+{
+    return await Users.getTime(phoneNumber);
+};
+
+const checkExistOrder= async(phoneNumber)=>
+{
+    return await Users.checkExistOrder(phoneNumber);
+};
 module.exports = {
     checkExistOrder,
     getAllOrders,
     getOrder,
+    cancelOrder,
+    getTime,
+    checkExistOrder,
 };
